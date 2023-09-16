84430cda567fb7e201032dfb060222e2e021e602
nandini965 <nandhini965@gmail.com>
2023-09-16 20:02:46 +0530

    filebeat

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "main" {
   tags = merge(var.tags, { Name = "${var.name}-${var.env}-sg" })
   }
 
-resource "aws_docdb_cluster_parameter_group" "main" {
+resource "aws_elasticache_parameter_group" "main" {
   family      = "redis6.x"
   name        = "${var.name}-${var.env}-pg"
   description = "${var.name}-${var.env}-pg"
